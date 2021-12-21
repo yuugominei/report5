eb9b4e355e503c99925325bf016fc48ed66252bb
yuugo minei <e215740@ie.u-ryukyu.ac.jp>
2021-12-21 15:16:52 +0900

    1st commit

--- app/src/main/java/report5/Null.java
+++ app/src/main/java/report5/Null.java
@@ -1,14 +1,16 @@
-package Report5;
+package report5;
 
 public class Null {
     
     
     public static void main (String[] args){
-        String str = null;
+        
         try {
-            System.out.println(str.length());
+        String str = null;
+        System.out.println(str.length());
     } catch (NullPointerException e){
         System.out.println("問題発生！");
+        System.out.println(e.getMessage());
     }
     }
 }
